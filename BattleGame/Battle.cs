ebbed46bc72738ad92d47ac770f30411b2215e59
Kate Sun <katesun@Kates-MacBook-Pro.local>
2018-05-21 14:02:14 -0400

    Initial Commit

---
+++ BattleGame/Battle.cs
@@ -0,0 +1,78 @@
+﻿using System;
+namespace BattleGame
+{
+    public class Battle
+    {
+        public Battle()
+        {
+			
+        }
+
+		// StartFight
+        // Receives Warrior1 Warrior2
+
+		public static void StartFight(Warrior warrior1, Warrior warrior2)
+        {
+
+			// Loop giving each warrior a chance to attack 
+			// and block each turn until one dies
+
+			while(true)
+			{
+				if(GetAttackResult(warrior1, warrior2) == "Game Over")
+				{
+					Console.WriteLine("Game Over");
+					break;
+				}
+
+				if (GetAttackResult(warrior2, warrior1) == "Game Over")
+				{
+					Console.WriteLine("Game Over");
+					break;
+				}
+			}
+
+
+        }
+
+		// GetAttackResult
+        // Receives WarriorA, WarriorB
+
+		public static string GetAttackResult(Warrior warriorA, Warrior warriorB) 
+		{
+			// Calculate 1 warrior's attack and the 
+			// others block
+			double attack = warriorA.Attack() - warriorB.Block();
+
+			if(attack > 0 && warriorB.Health - attack < 0) 
+			{
+				warriorB.Health = 0;
+			} else if (attack > 0 && warriorB.Health - attack > 0)
+			{
+				warriorB.Health -= attack;
+			} else attack = 0;
+
+			// Print out info on who attacked who and for how much damage
+			Console.WriteLine($"{warriorA.Name} attacked {warriorB.Name} with a damage of {attack}");
+
+
+			// Provide output on the change in health
+			Console.WriteLine($"{warriorB.Name}'s health has been reduced to {warriorB.Health} \n");
+
+
+            // Check if the warriors health fell below 0 and if so
+            // print a message and then send a response that will end the loop
+			if(warriorB.Health <= 0) 
+			{
+				Console.WriteLine($"{warriorB.Name} has died and {warriorA.Name} is victorious \n");
+				return "Game Over";
+			} 
+
+			return "";
+          
+
+		}
+        
+
+    }
+}
---
+++ BattleGame/Warrior.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace BattleGame
+{
+    public class Warrior
+    {
+		public string Name { get; set; } = "Warrior";
+		public double Health { get; set; } = 0;
+		public double AttackMax { get; set; } = 0;
+		public double BlockMax { get; set; } = 0;
+
+		// Instantiate Name, Health, Attack Max, Block Max
+        
+        public Warrior(string name, double health, double attackMax, double blockMax)
+        {
+			Name = name;
+			Health = health;
+			AttackMax = attackMax;
+			BlockMax = blockMax;
+        }
+
+		// Random numbers
+		Random random = new Random();
+
+        // Attack method
+		// Generate random attack from 1 to max attack
+		public double Attack() 
+		{
+			return random.Next(1, (int)AttackMax);
+		}
+
+        // Block method
+        // Generate random block from 1 to max block
+        public double Block()
+		{
+			return random.Next(1, (int)BlockMax);
+		}
+    }
+}
